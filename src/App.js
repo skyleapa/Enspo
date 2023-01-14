83d670d414cd6154ffc9252f42bbe95662d61f69
sarahhcheng <sarahcheng40@gmail.com>
2023-01-13 22:08:57 -0800

    added aritzia logo

--- src/App.js
+++ src/App.js
@@ -2,12 +2,27 @@ import React from "react";
 import SearchBar from "./components/SearchBar";
 import "./style.css";
 import Lookbook from "./components/lookbook";
+import Image from "./logo.png";
 
 class App extends React.Component {
   render() {
     return (
       <>
-        <header className="header"></header>
+        <header className="header">
+          <div logo-container>
+            <img src={require("./logo.png")} alt="test" id="logo-img"></img>
+          </div>
+
+          {/* <div class="flexbox-item">
+            <div class="nice">
+              <ul>
+                <li>Header 1</li>
+                <li>Header 2</li>
+                <li>Header 3</li>
+              </ul>
+            </div>
+          </div> */}
+        </header>
         <div className="ui container">
           <div className="welcome-container">
             <div className="welcome">Welcome to Aritzia Lookbook!</div>
