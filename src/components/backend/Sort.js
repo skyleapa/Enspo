1919696d30c62c3be2987d2c2184301f6653e3a1
Michelle Wang <54217965+skyleapa@users.noreply.github.com>
2023-01-14 11:11:44 -0800

    average of tags from openai

--- src/components/backend/Sort.js
+++ src/components/backend/Sort.js
@@ -1,46 +1,51 @@
 const { Configuration, OpenAIApi } = require("openai");
 
 const configuration = new Configuration({
-  apiKey: "test",
+  apiKey: "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",
 });
 const openai = new OpenAIApi(configuration);
 
 function stringer(arr) {
   theString = ""
-  for (element in array) {
-    theString += array;
+  for (let i = 0; i < arr.length;i++) {
+    theString = arr[i] + ", " + theString;
   }
-
   return theString
-
-  amounts.forEach(item => {
-    doubledAmounts.push(item * 2);
-  })
 }
 
-console.log(stringer(["sport", "book", "apple"]));
+async function respond(keyword, array) {
+    const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: "assign a numerical rating from 0 to 10 for how closely the following words relate to the word '" + keyword + "': '" + stringer(array) + "'. Return the average of these values." ,
+        temperature: 0.43,
+        max_tokens: 256,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+    });
 
+    // console.log("Response is " + response.data.choices[0].text);
 
-// // keyword = "academia";
-// // query = "assign a numerical rating from 0 to 10 for how closely the following words relate to the word '" + keyword + "': 'fitness, baggy, soft, books'. Return the words sorted by their values as a two dimensional array." 
-// async function respond(keyword, array) {
+    return response.data.choices[0].text;
   
-//     const response = await openai.createCompletion({
-//         model: "text-davinci-003",
-//         prompt: "assign a numerical rating from 0 to 10 for how closely the following words relate to the word '" + keyword + "': '" + stringer(array) + "'. Return the words sorted by their values as a two dimensional array." ,
-//         temperature: 0.43,
-//         max_tokens: 256,
-//         top_p: 1,
-//         frequency_penalty: 0,
-//         presence_penalty: 0,
-//     });
+}
 
-//     console.log("Response is " + response.data.choices[0].text);
+arrResponse = respond("beach", ["cool", "clear sky", "edgy"]);
 
-//     return response;
-// }
+console.log(arrResponse);
 
-// respond("academia", ["sport", "book", "apple"]);
+arrResponse.then(function(result) {
+  // console.log(result)
+  // console.log(typeof(result));
+  // console.log(final);
+  // console.log(typeof(final));
+  var final = parseInt(result);
+  return final;
+})
+
+
+
+
 
 
 
