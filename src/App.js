da00f8e35123df5b20be302b0aa863f3af1baa10
sarahhcheng <sarahcheng40@gmail.com>
2023-01-13 19:28:34 -0800

    Co-authored-by: daniCodes1 <daniCodes1@users.noreply.github.com> Co-authored-by: Michelle Wang <skyleapa@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import SearchBar from "./components/SearchBar";
+import "./style.css";
 
 class App extends React.Component {
   render() {
     return (
       <div className="ui container">
+        <div className="welcome">Welcome to Aritzia!</div>
         <SearchBar />
         <div>Hello hackers!</div>
       </div>
