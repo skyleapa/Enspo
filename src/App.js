261df9243f47385f36be4c3c65032715111ef420
sarahhcheng <sarahcheng40@gmail.com>
2023-01-13 20:20:20 -0800

    Co-authored-by: daniCodes1 <daniCodes1@users.noreply.github.com> Co-authored-by: Michelle Wang <skyleapa@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -6,9 +6,12 @@ class App extends React.Component {
   render() {
     return (
       <div className="ui container">
-        <div className="welcome">Welcome to Aritzia!</div>
+        <div className="welcome-container">
+          <div className="welcome">Welcome to Aritzia Lookbook!</div>
+          <hr />
+        </div>
+        <div className="slogan">Everyday luxury. </div>
         <SearchBar />
-        <div>Hello hackers!</div>
       </div>
     );
   }
