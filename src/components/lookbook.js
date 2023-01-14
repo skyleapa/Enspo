750eeb5f57f26f50eaf07bd25ae2a29515d26f37
sarahhcheng <sarahcheng40@gmail.com>
2023-01-13 23:17:42 -0800

    refactored from class-based code to functional

--- src/components/lookbook.js
+++ src/components/lookbook.js
@@ -1,8 +1,19 @@
 import React from "react";
 import ClothingPiece from "./ClothingPiece";
 
-const Lookbook = (props) => {
-  return <div className="content">{props.children}</div>;
+const Lookbook = () => {
+  return (
+    <div>
+      <ClothingPiece Image="" name="deedhdebh" tags="dedheddebh" />
+      <ClothingPiece Image="" name="" tags="" />
+      <ClothingPiece Image="" name="" tags="" />
+      <ClothingPiece Image="" name="" tags="" />
+      <ClothingPiece Image="" name="" tags="" />
+      <ClothingPiece Image="" name="" tags="" />
+    </div>
+  );
+
+  // return <div className="content">{props.children}</div>;
 };
 
 export default Lookbook;
