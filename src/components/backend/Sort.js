c9dbc35aca2108a72c1e705c1a2fb9e583d73a41
Michelle Wang <54217965+skyleapa@users.noreply.github.com>
2023-01-14 03:39:38 -0800

    helper

--- src/components/backend/Sort.js
+++ src/components/backend/Sort.js
@@ -1,25 +1,42 @@
 const { Configuration, OpenAIApi } = require("openai");
 
 const configuration = new Configuration({
-  apiKey: "hi",
+  apiKey: "butt",
 });
 const openai = new OpenAIApi(configuration);
 
-async function respond() {
-    const response = await openai.createCompletion({
-        model: "text-davinci-003",
-        prompt: "assign a numerical rating from 0 to 10 for how closely the following words relate to the word 'academia': 'fitness, baggy, soft, books'. Return the words sorted by their values as a two dimensional array.",
-        temperature: 0.43,
-        max_tokens: 256,
-        top_p: 1,
-        frequency_penalty: 0,
-        presence_penalty: 0,
-    });
+function stringer(array) {
+  theString = ""
+  for (element in array) {
+    theString += array;
+  }
 
-    console.log("Response is " + response.data.choices[0].text);
-
-    return response;
+  return theString
 }
 
-respond();
+console.log(stringer(["sport", "book", "apple"]));
+
+
+// // keyword = "academia";
+// // query = "assign a numerical rating from 0 to 10 for how closely the following words relate to the word '" + keyword + "': 'fitness, baggy, soft, books'. Return the words sorted by their values as a two dimensional array." 
+// async function respond(keyword, array) {
+  
+//     const response = await openai.createCompletion({
+//         model: "text-davinci-003",
+//         prompt: "assign a numerical rating from 0 to 10 for how closely the following words relate to the word '" + keyword + "': '" + stringer(array) + "'. Return the words sorted by their values as a two dimensional array." ,
+//         temperature: 0.43,
+//         max_tokens: 256,
+//         top_p: 1,
+//         frequency_penalty: 0,
+//         presence_penalty: 0,
+//     });
+
+//     console.log("Response is " + response.data.choices[0].text);
+
+//     return response;
+// }
+
+// respond("academia", ["sport", "book", "apple"]);
+
+
 
